380f6b67d12b0b89c1a6940d90ab9a67ffa4e69c
kimrunan <53895986+kimrunan@users.noreply.github.com>
2025-07-26 01:54:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+
+const formBaseUrl = "https://docs.google.com/forms/d/e/1FAIpQLSeUwKg5fH5LebqauOoRgm88KjTGLKXe1XjOkApEbwXYYvEkRw/viewform?usp=pp_url&entry.1234567890=";
+const sheetUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSWeVLX-_EP_glVBrHjj4FtpaSe0jhZyv06OQPruEiso18V_pIEON82yHUX2S1WE1RckemMIOFx9a-a/pub?output=csv";
+
+const seatPositions = [
+  [100, 100], [160, 100], [220, 100], [280, 100], [340, 100], [400, 100], [460, 100],
+  [100, 180], [160, 180], [220, 180], [280, 180], [340, 180], [400, 180], [460, 180]
+];
+
+function createSeat(num, isTaken) {
+  const seat = document.createElement(isTaken ? "div" : "a");
+  seat.textContent = num;
+  seat.className = `seat ${isTaken ? "opptatt" : "ledig"}`;
+  seat.style.top = seatPositions[num - 1][1] + "px";
+  seat.style.left = seatPositions[num - 1][0] + "px";
+  seat.style.position = "absolute";
+  if (!isTaken) {
+    seat.href = formBaseUrl + encodeURIComponent("Plass " + num);
+    seat.target = "_blank";
+  }
+  return seat;
+}
+
+fetch(sheetUrl)
+  .then(res => res.text())
+  .then(csv => {
+    const lines = csv.trim().split("\n");
+    const takenSeats = new Set();
+    for (let i = 1; i < lines.length; i++) {
+      const seatNum = parseInt(lines[i].split(",")[0].trim());
+      if (!isNaN(seatNum)) {
+        takenSeats.add(seatNum);
+      }
+    }
+
+    const container = document.getElementById("seats");
+    for (let i = 1; i <= 14; i++) {
+      container.appendChild(createSeat(i, takenSeats.has(i)));
+    }
+  });
